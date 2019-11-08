a591214a5c3870035e39bd1ff7ba893344db1611
zhouyixian-git <1125662823@qq.com>
2019-11-08 19:50:18 +0800

    initial commit

---
+++ application/controller/Index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/11/7 0007
+ * Time: 20:23
+ */
+
+namespace app\controller;
+
+
+use think\Controller;
+
+class Index extends Controller
+{
+    public function index(){
+        return $this->fetch();
+    }
+}
