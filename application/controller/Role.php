24b720013f5a448f76e69011d76f1c7b8200df85
zhouyixian-git <1125662823@qq.com>
2019-11-15 21:51:10 +0800

    角色管理

--- application/controller/Role.php
+++ application/controller/Role.php
@@ -9,30 +9,130 @@
 namespace app\controller;
 
 
+use app\model\Admin;
+use think\Db;
 use think\Request;
 
 class Role extends Base
 {
 
-    public function index(){
-        $roleList = \app\model\Role::order('role_id', 'desc')->select();
+    public function index(Request $request)
+    {
+
+        $role_code = $request->param('role_code');
+        $role_name = $request->param('role_name');
+        $where = [];
+        if (!empty($role_code)) {
+            $where[] = ['role_code', '=', $role_code];
+        }
+        if (!empty($role_name)) {
+            $where[] = ['role_name', '=', $role_name];
+        }
+
+        $roleList = Db::table('eas_role')
+            ->where($where)
+            ->order('role_id desc')
+            ->paginate(8, false, ['query' => $request->param(), 'type' => 'page\Page', 'var_page' => 'page']);
+
+        $page = $roleList->render();
+        $count = Db::table('eas_role')->where($where)->count();
+
+        $this->assign('role_code', $role_code);
+        $this->assign('role_name', $role_name);
+        $this->assign('page', $page);
+        $this->assign('count', $count);
         $this->assign('roleList', $roleList);
         return $this->fetch();
     }
 
-    public function add(){
+    public function add(Request $request)
+    {
+        if ($request->isPost()) {
+            $role_code = $request->post('role_code');
+            $role_name = $request->post('role_name');
+            $role_remark = $request->post('role_remark');
 
-    }
+            $data = [
+                'role_code' => $role_code,
+                'role_name' => $role_name,
+                'role_remark' => $role_remark
+            ];
+            $validate = new \app\validate\Role();
+            if (!$validate->check($data)) {
+                echo $this->errorJson(0, $validate->getError());
+                return;
+            }
 
-    public function edit(Request $request){
-        if($request->isPost()){
-            echo json_encode("");
-            exit;
+            $role = new \app\model\Role();
+            $role->save($data);
+            echo $this->successJson();
+            return;
         }
+        return $this->fetch();
     }
 
-    public function delete(){
+    public function edit(Request $request)
+    {
+        if ($request->isPost()) {
+            $role_id = $request->post('role_id');
+            $role_code = $request->post('role_code');
+            $role_name = $request->post('role_name');
+            $role_remark = $request->post('role_remark');
 
+            if (empty($role_id)) {
+                echo $this->errorJson(0, '关键数据错误');
+                exit;
+            }
+
+            $data = [
+                'role_code' => $role_code,
+                'role_name' => $role_name,
+                'role_remark' => $role_remark
+            ];
+            $validate = new \app\validate\Role();
+            if (!$validate->check($data)) {
+                echo $this->errorJson(0, $validate->getError());
+                return;
+            }
+
+            $role = new \app\model\Role();
+            $role->save($data, ['role_id' => $role_id]);
+            echo $this->successJson();
+            return;
+        }
+
+        $role_id = $request->get('role_id');
+        $role = \app\model\Role::where('role_id', $role_id)->find();
+
+        $this->assign('role', $role);
+        return $this->fetch();
+    }
+
+    public function delete(Request $request)
+    {
+        if ($request->isPost()) {
+            $role_id = $request->post('role_id');
+
+            if (empty($role_id)) {
+                echo $this->errorJson(0, '关键数据错误');
+                exit;
+            }
+
+            $count = Admin::where('role_id', $role_id)->count();
+            if ($count > 0) {
+                echo $this->errorJson(0, '该角色下存在管理员信息，不可删除！');
+                exit;
+            }
+
+            $affected = \app\model\Role::where('role_id', $role_id)->delete();
+            if ($affected > 0) {
+                echo $this->successJson();
+                exit;
+            } else {
+                echo $this->errorJson(0, '删除失败');
+                exit;
+            }
+        }
     }
 
 }
---
+++ application/model/Admin.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/11/15 0015
+ * Time: 19:31
+ */
+
+namespace app\model;
+
+
+use think\Model;
+
+class Admin extends Model
+{
+
+}
