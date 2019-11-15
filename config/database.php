f9f39a00d405b226d9e3646364efe9c85811e4f8
zhouyixian-git <1125662823@qq.com>
2019-11-15 22:22:51 +0800

    角色管理

--- config/database.php
+++ config/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => '49.235.87.218',
     // 数据库名
     'database'        => 'mrs',
     // 用户名
-    'username'        => 'root',
+    'username'        => 'mrs',
     // 密码
-    'password'        => '123456',
+    'password'        => 'mrs!23',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
